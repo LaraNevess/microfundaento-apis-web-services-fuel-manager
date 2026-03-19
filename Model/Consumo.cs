61d1e3065d01bdc30182d711e4379b5e825f6f37
LaraNevess <laraduda08@gmail.com>
2026-03-19 19:16:46 -0300

    Criação e configuração dos modelos de veiculo e consumo

---
+++ Model/Consumo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace microfundaento_apis_web_services_fuel_manager.Model
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public  string Descricão { get; set; }
+        [Required]
+        public  DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo{ get; set; }
+        [Required]
+        public int VeiculoId { get; set; }
+        public Veiculo Veiculo { get; set; }
+
+    }
+    public enum TipoCombustivel
+    {
+        Gasolina,
+        Etanol,
+        Diesel
+    }
+    
+}
